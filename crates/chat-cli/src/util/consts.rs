9e57dcb3832d8cce60cdc806a984943d83223a8c
Brandon Kiser <bskiser@amazon.com>
2025-05-16 06:36:27 -0700

    binary name

--- crates/chat-cli/src/util/consts.rs
+++ crates/chat-cli/src/util/consts.rs
@@ -3,7 +3,6 @@ pub const APP_PROCESS_NAME: &str = "q_desktop.exe";
 
 /// TODO(brandonskiser): revert back to "qchat" for prompting login after standalone releases.
 pub const CLI_BINARY_NAME: &str = "q";
-#[allow(dead_code)]
 pub const CHAT_BINARY_NAME: &str = "qchat";
 
 pub const PRODUCT_NAME: &str = "Amazon Q";
